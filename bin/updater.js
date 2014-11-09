ae73548efd5ee207c796f83a5fa56dc8dcf17db0
joje <joje.attrs@gmail.com>
2014-11-10 00:48:42 +0900

    0.0.5, add install,start option to bin/plexi.js

--- bin/updater.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(dir) {
-	console.log('bundle.system cli', dir);
-}
-
--- examples/start.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var plexi = require('../');
-
-plexi.cli.start();
-var app = plexi.startup(__dirname);
