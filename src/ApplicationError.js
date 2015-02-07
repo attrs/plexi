8ad203340fbe72d38d30498c2f535144abc73582
joje <joje.attrs@gmail.com>
2015-02-08 06:54:32 +0900

    ApplicationError fix

--- src/ApplicationError.js
+++ src/ApplicationError.js
@@ -1,19 +1,20 @@
-function ApplicationError( message, detail ) {
+function ApplicationError( message, cause ) {
 	Error.call(this, message);
 	this.name = 'ApplicationError';
 	this.message = message;
 	this.arguments = arguments;
-	this.detail = detail;
+	this.cause = cause;
 	
 	Error.captureStackTrace(this, arguments.callee);
 	
-	if( detail instanceof Error ) {
-		var stack = detail.stack.split(detail.name + ': ' + detail.message + '\n').join('');
+	if( cause instanceof Error ) {
+		var stack = cause.stack.split(cause.name + ': ' + cause.message + '\n').join('');
 		this.stack = this.name + (this.message ? ': ' + this.message : '') + '\n' + stack;
 	}
 }
 
-ApplicationError.prototype.__proto__ = Error.prototype;
-
+//ApplicationError.prototype.__proto__ = Error.prototype;
+ApplicationError.prototype = Object.create(Error.prototype);
+ApplicationError.prototype.constructor = ApplicationError;
 
 module.exports = ApplicationError;
